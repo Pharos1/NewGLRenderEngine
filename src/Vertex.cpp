a18e2a5e16a375b5b34471f086298a182dc3d48f
Pharos1 <73336596+Pharos1@users.noreply.github.com>
2023-08-01 14:05:59 +0300

    Function for setting attribute arrays

--- src/Vertex.cpp
+++ src/Vertex.cpp
@@ -1,10 +1,20 @@
 #include "pch.h"
 #include "Vertex.hpp"
 
-Vertex::Vertex(glm::vec3 pos, glm::vec3 normal, glm::vec2 texCoord)
-	: pos(pos), normal(normal), texCoord(texCoord){
+Vertex::Vertex(glm::vec3 pos, glm::vec3 normal, glm::vec2 texCoord, glm::vec3 tangent)
+	: pos(pos), normal(normal), texCoord(texCoord), tangent(tangent) {
 }
-Vertex::Vertex() {};
+
+void Vertex::setAttribArrays() {
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+	glEnableVertexAttribArray(3);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, pos)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, normal)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, texCoord)));
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, tangent)));
+}
 
 std::ostream& operator<<(std::ostream& os, const glm::vec4& other) {
 	os << other.x << ", " << other.y << ", " << other.z << ", " << other.w;
--- src/Vertex.hpp
+++ src/Vertex.hpp
@@ -4,12 +4,15 @@
 
 class Vertex {
 public:
-	glm::vec3 pos;
-	glm::vec3 normal;
-	glm::vec2 texCoord;
+	glm::vec3 pos{0};
+	glm::vec3 normal{0};
+	glm::vec2 texCoord{0};
+	glm::vec3 tangent{0};
 
-	Vertex(glm::vec3 pos, glm::vec3 normal, glm::vec2 texCoord);
-	Vertex();
+	Vertex(glm::vec3 pos, glm::vec3 normal = glm::vec3(0.f), glm::vec2 texCoord = glm::vec2(0.f), glm::vec3 tangent = glm::vec3(0.f));
+	Vertex() = default;
+
+	static void setAttribArrays();
 };
 std::ostream& operator<<(std::ostream& os, const glm::vec4& other);
 std::ostream& operator<<(std::ostream& os, const glm::vec3& other);
