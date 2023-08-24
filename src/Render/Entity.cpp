6e30b70815a6c8a145331b4194d61ee96746d6dc
Pharos1 <73336596+Pharos1@users.noreply.github.com>
2023-08-24 15:49:30 +0300

    Added Entity class and implemented scene graph functionality.

---
+++ src/Render/Entity.cpp
@@ -0,0 +1,72 @@
+#include "../pch.h"
+#include "Entity.hpp"
+#include "../Utilities/Time.hpp"
+
+//Transform
+glm::mat4 Transform::getLocalModelMat() {
+	glm::mat4 transformX = glm::rotate(glm::mat4(1.f), glm::radians(localRot.x), glm::vec3(1.f, 0.f, 0.f));
+	glm::mat4 transformY = glm::rotate(glm::mat4(1.f), glm::radians(localRot.y), glm::vec3(0.f, 1.f, 0.f));
+	glm::mat4 transformZ = glm::rotate(glm::mat4(1.f), glm::radians(localRot.z), glm::vec3(0.f, 0.f, 1.f));
+
+	glm::mat4 roationMatrix = transformX * transformY * transformZ;
+
+	return glm::translate(glm::mat4(1.f), localPos) * roationMatrix * glm::scale(glm::mat4(1.f), localScale);
+}
+
+void Transform::calcModelMatrix(const glm::mat4& parentGlobalModelMatrix) {
+	modelMat = parentGlobalModelMatrix * getLocalModelMat();
+	changed = false;
+}
+
+void Transform::setLocalPos(const glm::vec3 value) {
+	localPos = value;
+	changed = true;
+}
+void Transform::setLocalRot(const glm::vec3 value) {
+	localRot = value;
+	changed = true;
+}
+void Transform::setLocalScale(const glm::vec3 value) {
+	localScale = value;
+	changed = true;
+}
+
+glm::vec3 Transform::getLocalPos() const { return localPos; }
+glm::vec3 Transform::getLocalRot() const { return localRot; }
+glm::vec3 Transform::getLocalScale() const { return localScale; }
+glm::mat4 Transform::getGlobalModelMat() const { return modelMat; }
+
+bool Transform::isChanged() const { return changed; }
+
+//Entity
+void Entity::addChild(Entity& child) {
+	children.push_back(&child);
+	child.parent = this;
+}
+void Entity::updateSelf() {
+	if (transform.isChanged()) {
+		if (parent)
+			transform.calcModelMatrix(parent->transform.getGlobalModelMat());
+		else
+			transform.calcModelMatrix();
+	}
+}
+void Entity::updateSelfAndChild() {
+	updateSelf();
+
+	for (auto& child : children) {
+		child->updateSelfAndChild();
+	}
+}
+void Entity::draw(const Shader& shader) {
+	updateSelf();
+
+	shader.use();
+	shader.setMat4("model", transform.getGlobalModelMat());
+
+	model.draw();
+
+	for (auto& child : children) {
+		child->draw(shader);
+	}
+}
---
+++ src/Render/Entity.hpp
@@ -0,0 +1,45 @@
+#include "../pch.h"
+#include "Model.hpp"
+
+class Transform {
+private:
+	glm::vec3 localPos = glm::vec3(0.f);
+	glm::vec3 localRot = glm::vec3(0.f);
+	glm::vec3 localScale = glm::vec3(1.f);
+
+	//Global space
+	glm::mat4 modelMat = glm::mat4(1.f);
+
+	bool changed = true;
+
+public:
+	glm::mat4 getLocalModelMat();
+
+	void calcModelMatrix(const glm::mat4& parentGlobalModelMatrix = glm::mat4(1.f));
+
+	void setLocalPos(const glm::vec3 value);
+	void setLocalRot(const glm::vec3 value);
+	void setLocalScale(const glm::vec3 value);
+
+	glm::vec3 getLocalPos() const;
+	glm::vec3 getLocalRot() const;
+	glm::vec3 getLocalScale() const;
+	glm::mat4 getGlobalModelMat() const;
+
+	bool isChanged() const;
+};
+
+class Entity {
+public:
+	Model model;
+	Transform transform;
+	std::vector<Entity*> children; //Note: could also make the entity own the vectors on the stack.
+	Entity* parent = nullptr;
+
+	Entity() = default;
+
+	void addChild(Entity& child);
+	void updateSelf();
+	void updateSelfAndChild();
+	void draw(const Shader& shader);
+};
