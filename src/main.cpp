e7a3c0ddf494377422a691072894944d5a21876d
Pharos1 <73336596+Pharos1@users.noreply.github.com>
2023-08-24 16:22:22 +0300

    Added GUI with a few options.

--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,14 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 void initDependencies();
 void setupApplication();
 void setupScreenRelated();
+void initImGui();
 void setupUBOs();
 void cleanup();
 
 //Every frame
 void draw(const Shader& shader);
 void renderScene(const Shader& shader);
+void updateGUI();
 
 //Application Specific
 uint32_t scrWidth = 1280;
@@ -177,6 +179,48 @@ std::vector<Vertex> planeVerts = {
 Query depthPassQuery;
 Query renderPassQuery;
 Query postprocQuery;
+Query guiPassQuery;
+
+//GUI
+ImGuiIO io;
+ImGuiStyle* style;
+float windowAlpha;
+
+//UI Options
+int currentStyle = 0; //0 - Classic, 1 - Dark, 2 - Light
+float bgAlpha = .7f;
+
+//Software and Hardwere Info
+std::string openGLVersion;
+std::string glslVersion;
+std::string vendor;
+std::string gpuVersion;
+
+//OpenGL Options
+bool vsyncOn = false;
+bool mouseLocked = true;
+int msaaSamples = 4;
+bool msaaEnabled = true;
+int msaaMaxSamples = 0;
+
+//Post-processing Options
+float exposure = 2.2f;
+float gamma = 2.2f;
+bool gammaOn = true;
+int tonemapMode = 5;
+float maxRadiance = 1.f;
+
+//FXAA
+#define DEFAULT_EDGE_THRESHOLD_MIN .0312f;
+#define DEFAULT_EDGE_THRESHOLD_MAX .125f;
+#define DEFAULT_ITERATIONS 12;
+#define DEFAULT_SUBPIXEL_QUALITY .25f;
+float EDGE_THRESHOLD_MIN = DEFAULT_EDGE_THRESHOLD_MIN;
+float EDGE_THRESHOLD_MAX = DEFAULT_EDGE_THRESHOLD_MAX;
+int ITERATIONS = DEFAULT_ITERATIONS;
+float SUBPIXEL_QUALITY = DEFAULT_SUBPIXEL_QUALITY;
+bool fxaaRevert = false;
+bool fxaaEnabled = true;
 
 //UBOs
 GLuint uboMatrices;
@@ -197,7 +241,7 @@ int main() {
 			depthPassQuery.retrieveResult();
 			renderPassQuery.retrieveResult();
 			postprocQuery.retrieveResult();
-
+			guiPassQuery.retrieveResult();
 			double depthTime = (double)depthPassQuery.getResult() / 1000000;
 			double renderTime = (double)renderPassQuery.getResult() / 1000000;
 			double postprocTime = (double)postprocQuery.getResult() / 1000000;
@@ -228,20 +272,15 @@ int main() {
 
 		postprocShader.use();
 
-		if (glfwGetKey(window, GLFW_KEY_0)) postprocShader.set1i("tonemapMode", 0);
-		if (glfwGetKey(window, GLFW_KEY_1)) postprocShader.set1i("tonemapMode", 1);
-		if (glfwGetKey(window, GLFW_KEY_2)) postprocShader.set1i("tonemapMode", 2);
-		if (glfwGetKey(window, GLFW_KEY_3)) postprocShader.set1i("tonemapMode", 3);
-		if (glfwGetKey(window, GLFW_KEY_4)) postprocShader.set1i("tonemapMode", 4);
-		if (glfwGetKey(window, GLFW_KEY_5)) postprocShader.set1i("tonemapMode", 5);
-		if (glfwGetKey(window, GLFW_KEY_6)) postprocShader.set1i("tonemapMode", 6);
+		if (glfwGetKey(window, GLFW_KEY_0)) tonemapMode = 0;
+		if (glfwGetKey(window, GLFW_KEY_1)) tonemapMode = 1;
+		if (glfwGetKey(window, GLFW_KEY_2)) tonemapMode = 2;
+		if (glfwGetKey(window, GLFW_KEY_3)) tonemapMode = 3;
+		if (glfwGetKey(window, GLFW_KEY_4)) tonemapMode = 4;
+		if (glfwGetKey(window, GLFW_KEY_5)) tonemapMode = 5;
+		if (glfwGetKey(window, GLFW_KEY_6)) tonemapMode = 6;
 
-		if (!glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1)) spotLight.setColor(glm::vec3(0));
-		else spotLight.setColor(glm::vec3(80));
-		if (!glfwGetKey(window, GLFW_KEY_F)) dirLight.setColor(glm::vec3(0));
-		else dirLight.setColor(glm::vec3(2));
-		if (!glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2)) pointLight.setColor(glm::vec3(0));
-		else pointLight.setColor(glm::vec3(10));
+		postprocShader.set1i("tonemapMode", tonemapMode);
 
 		spotLight.set("spotLight", mainShader);
 		dirLight.set("dirLight", mainShader);
@@ -360,15 +399,6 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
 
 	proj = glm::perspective(glm::radians(fov), (float)scrWidth / scrHeight, nearPlane, farPlane);
 	
-	mainShader.use();
-	mainShader.setMat4("proj", proj);
-
-	lightBoxShader.use();
-	lightBoxShader.setMat4("proj", proj);
-
-	depthPrePassShader.use();
-	depthPrePassShader.setMat4("proj", proj);
-
 	//Recreate framebuffers
 	setupScreenRelated();
 	setupUBOs(); //For the proj
@@ -418,9 +448,7 @@ void initDependencies() {
 		nLog("Failed to create a window", Log::LogError, "");
 	}
 	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-	glfwSetKeyCallback(window, keyCallback);
-	glfwSwapInterval(0);
+	glfwSwapInterval(vsyncOn);
 	//if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) { //Normal GLAD
 		//if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) { //GLAD2
 	//	mLog("Failed to initialize GLAD!", Log::LogError);
@@ -443,9 +471,19 @@ void initDependencies() {
 			0, nullptr, GL_TRUE);
 	}
 
+	//Software and Hardware Info
+	openGLVersion = (char*)glGetString(GL_VERSION);
+	glslVersion = (char*)glGetString(GL_SHADING_LANGUAGE_VERSION);
+	vendor = (char*)glGetString(GL_VENDOR);
+	gpuVersion = (char*)glGetString(GL_RENDERER);
+
 	//Configure GL options
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	glEnable(GL_DEPTH_TEST);
+
+	glfwWindowHint(GLFW_SAMPLES, msaaSamples);
+	if (msaaEnabled) glEnable(GL_MULTISAMPLE);
+	glGetIntegerv(GL_MAX_INTEGER_SAMPLES, &msaaMaxSamples);
 
 	//Face cull
 	glEnable(GL_CULL_FACE);
@@ -516,9 +554,9 @@ void setupApplication() {
 	//ballModel.meshes[0].material.albedo.loadTexture("Textures/Debug/white.png");
 
 	//Lights
-	dirLight = DirLight({ -1.f, -1.f, -1.f }, glm::vec3(5.f * 0));
-	pointLight = PointLight({ 0.f, .20f, .8f }, glm::vec3((10.f + 5) * 1));
-	spotLight = SpotLight(cam.pos, cam.front, glm::vec3((20.f + 60.f) * 1), glm::cos(glm::radians(12.5f)), glm::cos(glm::radians(15.f)));
+	dirLight = DirLight({ -1.f, -1.f, -1.f }, glm::vec3(4.f), false);
+	pointLight = PointLight({ 0.f, .20f, .8f }, glm::vec3((10.f + 5)), false);
+	spotLight = SpotLight(cam.pos, cam.front, glm::vec3((20.f + 60.f)), glm::cos(glm::radians(12.5f)), glm::cos(glm::radians(15.f)), false);
 
 	//Textures
 	woodTexture.addTexture("Textures/Other/Wood.png", 0);
@@ -545,9 +583,18 @@ void setupApplication() {
 	lightBoxShader.setMat4("proj", proj);
 
 	postprocShader.use();
-	postprocShader.set1b("gammaOn", true);
-	postprocShader.set1f("gamma", 2.2f);
-	postprocShader.set1i("tonemapMode", 5);
+	postprocShader.set1b("gammaOn", gammaOn);
+	postprocShader.set1b("fxaaEnabled", fxaaEnabled);
+	postprocShader.set1f("exposure", exposure);
+	postprocShader.set1f("gamma", gamma);
+	postprocShader.set1f("maxRadiance", maxRadiance);
+	postprocShader.set1i("tonemapMode", tonemapMode);
+
+	postprocShader.setVec2("inverseScreenSize", glm::vec2(1.f / scrWidth, 1.f / scrHeight));
+	postprocShader.set1f("EDGE_THRESHOLD_MIN", EDGE_THRESHOLD_MIN);
+	postprocShader.set1f("EDGE_THRESHOLD_MAX", EDGE_THRESHOLD_MAX);
+	postprocShader.set1i("ITERATIONS", ITERATIONS);
+	postprocShader.set1f("SUBPIXEL_QUALITY", SUBPIXEL_QUALITY);
 
 	//Queries
 	depthPassQuery.loadQuery(GL_TIME_ELAPSED);
@@ -561,7 +608,29 @@ void setupApplication() {
 
 	//Queries
 	renderPassQuery.loadQuery(GL_TIME_ELAPSED);
+}
+void setupScreenRelated() {
+	//Framebuffers
+	postprocFB.create2D(scrWidth, scrHeight, GL_RGBA16F);
+}
+void initImGui(){
+	std::cout << "[                            MAKE UBOs work to share matrices between the shaders]\n";
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	//ImGuiIO& io = GetIO(); (void)io;
+	//StyleColorsDark();
 
+	//Style
+	if (currentStyle == 0) ImGui::StyleColorsClassic();
+	else if (currentStyle == 1) ImGui::StyleColorsDark();
+	else if (currentStyle == 2) ImGui::StyleColorsLight();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 450");
+	style = &ImGui::GetStyle();
+	windowAlpha = style->Alpha;
+
+	style->Alpha = .3f;
 }
 void setupUBOs() {
 	if (!uboMatrices) {
@@ -579,11 +648,27 @@ void setupUBOs() {
 	glBindBuffer(GL_UNIFORM_BUFFER, 0);
 }
 void cleanup() {
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
 	glfwTerminate();
 }
 
 //Every frame
 void draw(const Shader& shader) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+	glDepthFunc(GL_LEQUAL);
+
+	//Draw
+	postprocFB.bind();
+	postprocFB.clear();
+
+	depthPassQuery.begin();
+	renderScene(depthPrePassShader);
+	depthPassQuery.end();
+
+	renderPassQuery.begin();
 	renderScene(shader);
 
 	//Light cube pass
@@ -598,6 +683,10 @@ void draw(const Shader& shader) {
 	gradientSkyboxShader.setMat4("view", view);
 
 	quad.draw();
+
+	guiPassQuery.begin();
+	updateGUI();
+	guiPassQuery.end();
 }
 void renderScene(const Shader& shader) {
 	shader.use();
@@ -636,4 +725,190 @@ void renderScene(const Shader& shader) {
 	woodTexture.bind();
 	cube.draw();
 	woodTexture.unbind();
+}
+void updateGUI() {
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+
+	io = ImGui::GetIO();
+
+	ImGuiStyle& style = ImGui::GetStyle();
+
+	ImGui::SetNextWindowSizeConstraints(ImVec2(200, scrHeight), ImVec2(scrWidth, scrHeight));
+	ImGui::SetNextWindowBgAlpha(bgAlpha);
+
+	ImGui::Begin("Option Window", NULL, ImGuiWindowFlags_NoMove);
+	ImGui::SetWindowPos(ImVec2(scrWidth - ImGui::GetWindowWidth(), 0));
+
+	if (ImGui::CollapsingHeader("Rendering", ImGuiTreeNodeFlags_DefaultOpen)) {
+		if (ImGui::TreeNode("Post-processing")) {
+			ImGui::Text("Anti-Aliasing");
+			if (ImGui::Checkbox("FXAA", &fxaaEnabled)) {
+				postprocShader.use();
+				postprocShader.set1b("fxaaEnabled", fxaaEnabled);
+			}
+
+			ImGui::NewLine();
+
+			ImGui::Text("FXAA Options");
+			ImGui::BeginDisabled(!fxaaEnabled);
+			if (ImGui::SliderFloat("FXAA_EDGE_THRESHOLD_MIN", &EDGE_THRESHOLD_MIN, 0, 1, "%.4f")) {
+				postprocShader.use();
+				postprocShader.set1f("EDGE_THRESHOLD_MIN", EDGE_THRESHOLD_MIN);
+				fxaaRevert = true;
+			}
+			if (ImGui::SliderFloat("FXAA_EDGE_THRESHOLD_MAX", &EDGE_THRESHOLD_MAX, 0, 1, "%.4f")) {
+				postprocShader.use();
+				postprocShader.set1f("EDGE_THRESHOLD_MAX", EDGE_THRESHOLD_MAX);
+				fxaaRevert = true;
+			}
+			if (ImGui::SliderInt("FXAA_ITERATIONS", &ITERATIONS, 0, 100)) {
+				postprocShader.use();
+				postprocShader.set1i("ITERATIONS", ITERATIONS);
+				fxaaRevert = true;
+			}
+			if (ImGui::SliderFloat("FXAA_SUBPIXEL_QUALITY", &SUBPIXEL_QUALITY, 0, 1, "%.4f")) {
+				postprocShader.use();
+				postprocShader.set1f("SUBPIXEL_QUALITY", SUBPIXEL_QUALITY);
+				fxaaRevert = true;
+			}
+			if (fxaaRevert) {
+				if (ImGui::Button("Revert")) {
+					EDGE_THRESHOLD_MIN = DEFAULT_EDGE_THRESHOLD_MIN;
+					EDGE_THRESHOLD_MAX = DEFAULT_EDGE_THRESHOLD_MAX;
+					ITERATIONS = DEFAULT_ITERATIONS;
+					SUBPIXEL_QUALITY = DEFAULT_SUBPIXEL_QUALITY;
+
+					postprocShader.use();
+					postprocShader.set1f("EDGE_THRESHOLD_MIN", EDGE_THRESHOLD_MIN);
+					postprocShader.set1f("EDGE_THRESHOLD_MAX", EDGE_THRESHOLD_MAX);
+					postprocShader.set1i("ITERATIONS", ITERATIONS);
+					postprocShader.set1f("SUBPIXEL_QUALITY", SUBPIXEL_QUALITY);
+
+					fxaaRevert = false;
+				}
+			}
+			ImGui::EndDisabled();
+
+			ImGui::NewLine();
+
+			ImGui::Text("Tonemapping");
+			std::vector<const char*> tonemapModes = { "None", "Reinhard", "Extended Reinhard", "Uncharted2", "Hill ACES", "Narkowicz ACES", "Manual Exposure" };
+
+			if (ImGui::Combo("Tonemap Mode", &tonemapMode, tonemapModes.data(), tonemapModes.size())) {
+				postprocShader.use();
+				postprocShader.set1i("tonemapMode", tonemapMode);
+			}
+			ImGui::NewLine();
+
+			if (ImGui::SliderFloat("Maximum Radiance", &maxRadiance, 0, 10)) {
+				postprocShader.use();
+				postprocShader.set1f("maxRadiance", maxRadiance);
+			}
+			if (ImGui::SliderFloat("Manual Exposure", &exposure, 0, 10)) {
+				postprocShader.use();
+				postprocShader.set1f("exposure", exposure);
+			}
+			
+
+			ImGui::Text("Gamma Correction");
+			if (ImGui::Checkbox("Use Gamma Correction", &gammaOn)) {
+				postprocShader.use();
+				postprocShader.set1b("gammaOn", gammaOn);
+			}
+
+			ImGui::BeginDisabled(!gammaOn);
+			if (ImGui::SliderFloat("Gamma", &gamma, 0.f, 10.f)) {
+				postprocShader.use();
+				postprocShader.set1f("gamma", gamma);
+			}
+
+			ImGui::EndDisabled();
+			
+			ImGui::TreePop();
+		}
+	}
+	if (ImGui::CollapsingHeader("Shaders")) {
+		if (ImGui::Button("Reload")) {
+			mainShader.loadShader("src/Shaders/main.vert", "src/Shaders/main.frag");
+
+			mainShader.use();
+			dirLight.set("dirLight", mainShader);
+			pointLight.set("pointLights", mainShader);
+			spotLight.set("spotLight", mainShader);
+		}
+		ImGui::SameLine(); ImGui::Text("Main Shader");
+		
+		if (ImGui::Button("Reload##0")) {
+			postprocShader.loadShader("src/Shaders/basicQuad.vert", "src/Shaders/postProc.frag");
+
+			postprocShader.use();
+			postprocShader.set1b("gammaOn", gammaOn);
+			postprocShader.set1b("fxaaEnabled", fxaaEnabled);
+			postprocShader.set1f("exposure", exposure);
+			postprocShader.set1f("gamma", gamma);
+			postprocShader.set1f("maxRadiance", maxRadiance);
+			postprocShader.set1i("tonemapMode", tonemapMode);
+
+			postprocShader.setVec2("inverseScreenSize", glm::vec2(1.f / scrWidth, 1.f / scrHeight));
+			postprocShader.set1f("EDGE_THRESHOLD_MIN", EDGE_THRESHOLD_MIN);
+			postprocShader.set1f("EDGE_THRESHOLD_MAX", EDGE_THRESHOLD_MAX);
+			postprocShader.set1i("ITERATIONS", ITERATIONS);
+			postprocShader.set1f("SUBPIXEL_QUALITY", SUBPIXEL_QUALITY);
+		}
+		ImGui::SameLine(); ImGui::Text("Post-processing Shader");
+	}
+	if (ImGui::CollapsingHeader("UI Options")) {
+		ImGui::SliderFloat("BG Alpha", &bgAlpha, 0.f, 1.f);
+
+		std::vector<const char*> styles = { "Classic", "Dark", "Light" };
+		if (ImGui::Combo("Style", &currentStyle, styles.data(), styles.size())) {
+			if (currentStyle == 0) ImGui::StyleColorsClassic();
+			else if (currentStyle == 1) ImGui::StyleColorsDark();
+			else if (currentStyle == 2) ImGui::StyleColorsLight();
+		}
+	}
+	if (ImGui::CollapsingHeader("Profiling", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGui::Text(("ImGui Average framerate: " + std::to_string((int)io.Framerate) + " FPS").c_str());
+			ImGui::Text(("ImGui Average frametime: " + std::to_string(1000 / io.Framerate) + " ms").c_str());
+			ImGui::Text(("Average Delta Time:      " + std::to_string(Time::avgMsTime) + " ms").c_str());
+			ImGui::NewLine();
+
+			ImGui::Text("GPU Times [Queries]");
+			double gpuFrametime = (depthPassQuery.getResult() + renderPassQuery.getResult() + guiPassQuery.getResult() + postprocQuery.getResult()) / 1000000.0;
+			//Text(("Approx. CPU time: " + std::to_string(1000 / io.Framerate - gpuFrametime) + " ms").c_str());
+			ImGui::Text(("GPU time: " + std::to_string(gpuFrametime) + " ms").c_str());
+			ImGui::NewLine();
+
+			ImGui::Text(("Depth Pass:     " + std::to_string(depthPassQuery.getResult() / 1000000.0) + "  ms").c_str());
+			ImGui::Text(("Render Pass:    " + std::to_string(renderPassQuery.getResult() / 1000000.0) + "  ms").c_str());
+			ImGui::Text(("GUI Pass:       " + std::to_string(guiPassQuery.getResult() / 1000000.0) + "  ms").c_str());
+			ImGui::Text(("Post-Proc Pass: " + std::to_string(postprocQuery.getResult() / 1000000.0) + "  ms").c_str());
+		}
+	if (ImGui::CollapsingHeader("Software & Hardware Info")) {
+		ImGui::Text(("OpenGL Version: " + openGLVersion).c_str());
+		ImGui::Text(("GLSL Version: " + glslVersion).c_str());
+		ImGui::Text(("Vendor: " + vendor).c_str());
+		ImGui::Text(("GPU Version: " + gpuVersion).c_str());
+	}
+	if (ImGui::CollapsingHeader("OpenGL Options")) {
+		if (ImGui::Checkbox("VSync", &vsyncOn)) {
+			glfwSwapInterval(vsyncOn);
+		}
+		if (ImGui::Checkbox("MSAA", &msaaEnabled)) {
+			if (msaaEnabled) glEnable(GL_MULTISAMPLE);
+			else glDisable(GL_MULTISAMPLE);
+		} ImGui::SameLine();
+		if (ImGui::SliderInt("##0", &msaaSamples, 1, msaaMaxSamples)) {
+			glfwWindowHint(GLFW_SAMPLES, msaaSamples);
+		}
+	}
+
+	ImGui::End();
+	//ImGui::ShowMetricsWindow();
+	//ImGui::ShowDemoWindow();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
